972e39a30daab07c10fee66f6600dc6718e18e98
Erick Stev ESGM <esgomez2@espe.edu.ec>
2023-06-01 21:37:33 -0500

    Alfa 1.0.1.3

--- src/Gestor.java
+++ src/Gestor.java
@@ -6,11 +6,11 @@ public class Gestor extends Movimientos {
 
     //Metodo que permite ver el historial de movimientos realizados
     public void vermovi(){
-
+        //Proximamente V2.1
     }
 
     //Permite ver patrimonio actualizado
     public void verpatri(){
-
+        //Proximamente V2.1
     }
 }
